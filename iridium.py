0be015405e5b40c144700ca6821e74bf263a4f97
alexandr klimov <avlk.klimov@gmail.com>
2018-08-25 09:43:41 +0300

    move file saving to single function

--- iridium.py
+++ iridium.py
@@ -27,19 +27,29 @@ def checksum(bf):
 	data = bf[0:len(bf)-sum_size]
 	cs_byte=bf[-2:]
 	calc_cs = sum(data)
-	#print(len(buff))
-	#print(data)
-	#print(len(data))
-	#print("CALC_CS %s" % hex(calc_cs))
 	cs_int = int(cs_byte[0]) * 256 + int( cs_byte[1] )
-	#print(hex(calc_cs))
-	#print(hex(cs_int))
 	if calc_cs == cs_int:
 		return True
 	else:
 		return False  
 
-
+def buff_to_file(raw_buf):
+	#сохраняем в файл
+	ff=datetime.strftime(datetime.now() ,"%Y-%m-%d_%H-%M-%S")
+	filename = bin_folder+ff+'.bin';
+	f=open(filename, 'bw')
+	f.write( raw_buf )
+	f.close();
+def write_ok():
+	ser.write(b'OK\r')
+def show_buff(raw):
+	s=''
+	z=0
+	for b in raw:
+		s = s + str(z) + ':' + str(hex(b)) + ' '
+		z = z + 1 
+	print(s)
+	
 #ищем в символьном буфере сигнатуру команды
 def cmd_type():
 	AT = buff.find(b'AT\r')
@@ -85,7 +95,6 @@ while True:
 			print("--> OK\n")
 			ser.write(b'OK\r')
 		if cmd == SBDD2_C:
-			#print("SBDD2")
 			print("--> OK\n")
 			ser.write(b'0\rOK\r')
 		if cmd == SBDD0_C:
@@ -100,44 +109,30 @@ while True:
 		if cmd == SBDWB_C:
 			print(buff)
 			#выделяем символы между BDWB= и символом \r это размер данных
-			len_s = buff.find(b'BDWB=')
-			len_e = buff.find(b'\r')
-			#print( "START LENGTH: %s" % str(len_s+5) )
-			#print( "END LENGTH: %s" % str(len_e) )
-			# тут всегда будет верно, а len_s нам нужно для вырезания длины сообщения, надо бы переписать
-			if len_s != -1:
-				#сколько символов занимает длина пакета
-                                #len_s +5  -- позиция после знака равно в команде BDWB 
-				#
-				length_data = len_e - len_s - 5
-				#print( "DATA LENGTH%s" % str(length_data) )
-				length_read=buff[len_s+5:len_e]
-				#print(length_read)
-				#длина сообщения есть параметр команды BDWB и еще 2 байта
-				msg_len = int(length_read)  + 2
-				print ("--> READY\n")
-				ser.write(b'READY\r')
-				#читаем сколько нужно байт и еще два для CRC
-				raw = ser.read( msg_len)
-				print("length raw %s" % len(raw))
-				s=''
-				z=0
-				for b in raw:
-					s = s + str(z) + ':' + str(hex(b)) + ' '
-					z = z + 1 
-				print(s)
-				#сохраняем в файл
-				ff=datetime.strftime(datetime.now() ,"%Y-%m-%d_%H-%M-%S")
-				filename = bin_folder+ff+'.bin';
-
-				f=open(filename, 'bw')
-				f.write( raw )
-				f.close();
-				if checksum(raw):
-					print("CHECKSUM CORRECT")
-					ser.write(b'0\rOK\r')
-				else:
-					print("CHECKSUM INCORRECT")
-					ser.write(b'2\rOK\r')
+			
+			cmd_length = len('BDWB=')
+			position_s = buff.find(b'BDWB=')
+			position_e = buff.find(b'\r')
+			length_data = position_e - position_s - cmd_length
+			#print( "DATA LENGTH%s" % str(length_data) )
+			length_read=buff[position_s + cmd_length : position_e]
+			#длина сообщения есть параметр команды BDWB и еще 2 байта
+			CRC_LEN = 2
+			msg_len = int(length_read)  + CRC_LEN
+			print ("--> READY\n")
+			ser.write(b'READY\r')
+			#читаем сколько нужно байт и еще два для CRC
+			raw = ser.read( msg_len)
+			print("length raw %s" % len(raw))
+			buff_to_file(raw)
+			show_buff(raw)
+			if checksum(raw):
+				print("CHECKSUM CORRECT")
+				ser.write(b'0\r')
+				write_ok()
+			else:
+				print("CHECKSUM INCORRECT")
+				ser.write(b'2\r')
+				write_ok()
 		#обнуляем глобальную переменную буфер		
 		buff = b''               
