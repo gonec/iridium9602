e228c9b83d9e78bd12cb001bb4c377dfa7607f95
alexandr klimov <avlk.klimov@gmail.com>
2018-09-04 16:28:36 +0300

    add process start

---
+++ install.bat
@@ -0,0 +1,3 @@
+pip3 install pyserial
+pip3 install configparser
+pip3 install requests
---
+++ start.py
@@ -0,0 +1,4 @@
+from subprocess import Popen
+proc1=Popen("python iridium.py",shell=True)
+
+proc2=Popen("python netsender.py",shell=True)
